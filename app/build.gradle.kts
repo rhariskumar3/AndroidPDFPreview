7b205c4459253f3784362e5fe1903550328350b8
Haris SK <r.hariskumar3@gmail.com>
2023-12-01 15:59:22 +0530

    #change AGP bumped to stable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,10 +40,8 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
 
     implementation(project(":pdfpreview"))
 }
