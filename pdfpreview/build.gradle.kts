f31738d15e69ff753794971be9b87c5049cbc017
Haris SK <r.hariskumar3@gmail.com>
2023-12-15 16:44:34 +0530

    #new Configure Publish package to the Maven Central Repository

--- pdfpreview/build.gradle.kts
+++ pdfpreview/build.gradle.kts
@@ -1,6 +1,11 @@
+import org.jreleaser.model.Active
+
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("org.jreleaser") version "1.9.0"
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -29,6 +34,89 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    publishing {
+        singleVariant("release") {
+            withJavadocJar()
+            withSourcesJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = "io.github.rhariskumar3"
+            artifactId = "pdfpreview"
+            version = "1.0.0-SNAPSHOT"
+
+            pom {
+                name.set("Android PDF Preview")
+                description.set("AndroidPDFPreview is a lightweight and easy-to-use SDK that enables you to display and interact with PDF documents in your Android apps")
+                url.set("https://github.com/rhariskumar3/AndroidPDFPreview")
+                packaging = "aar"
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/rhariskumar3/AndroidPDFPreview.git")
+                    developerConnection.set("scm:git:ssh://github.com/rhariskumar3/AndroidPDFPreview.git")
+                    url.set("https://github.com/rhariskumar3/AndroidPDFPreview")
+                    tag.set("HEAD")
+                }
+                developers {
+                    developer {
+                        id.set("harissk")
+                        name.set("Haris")
+                        email.set("r.hariskumar3@gmail.com")
+                    }
+                }
+            }
+
+            afterEvaluate {
+                from(components.getByName("release"))
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+        }
+    }
+}
+
+jreleaser {
+    project {
+        copyright.set("Haris")
+    }
+    gitRootSearch.set(true)
+    signing {
+        active.set(Active.ALWAYS)
+        armored.set(true)
+    }
+    deploy {
+        maven {
+            nexus2 {
+                create("maven-central") {
+                    active.set(Active.ALWAYS)
+                    url.set("https://s01.oss.sonatype.org/service/local")
+                    snapshotUrl.set("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+                    closeRepository.set(false)
+                    releaseRepository.set(false)
+                    stagingRepositories.add("build/staging-deploy")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
 
 dependencies {
