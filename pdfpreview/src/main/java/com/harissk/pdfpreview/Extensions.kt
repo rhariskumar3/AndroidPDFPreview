2fa48eac48995285fe88ad30d65972ffaa83bd20
Haris SK <r.hariskumar3@gmail.com>
2023-12-20 23:01:11 +0530

    #optimize Simplify source methods using DocumentSource.toDocumentSource()

--- pdfpreview/src/main/java/com/harissk/pdfpreview/Extensions.kt
+++ pdfpreview/src/main/java/com/harissk/pdfpreview/Extensions.kt
@@ -2,12 +2,7 @@ package com.harissk.pdfpreview
 
 import android.net.Uri
 import com.harissk.pdfpreview.request.PdfRequest
-import com.harissk.pdfpreview.source.AssetSource
-import com.harissk.pdfpreview.source.ByteArraySource
 import com.harissk.pdfpreview.source.DocumentSource
-import com.harissk.pdfpreview.source.FileSource
-import com.harissk.pdfpreview.source.InputStreamSource
-import com.harissk.pdfpreview.source.UriSource
 import java.io.File
 import java.io.InputStream
 
@@ -28,31 +23,31 @@ inline fun PDFView.load(
 }
 
 /** Use an asset file as the pdf source  */
-fun PDFView.load(
+inline fun PDFView.load(
     assetName: String,
     builder: PdfRequest.Builder.() -> Unit = {},
-) = load(AssetSource(assetName), builder)
+) = load(DocumentSource.toDocumentSource(assetName), builder)
 
 /** Use a file as the pdf source  */
-fun PDFView.load(
+inline fun PDFView.load(
     file: File,
     builder: PdfRequest.Builder.() -> Unit = {},
-) = load(FileSource(file), builder)
+) = load(DocumentSource.toDocumentSource(file), builder)
 
 /** Use URI as the pdf source, for use with content providers  */
-fun PDFView.load(
+inline fun PDFView.load(
     uri: Uri,
     builder: PdfRequest.Builder.() -> Unit = {},
-) = load(UriSource(uri), builder)
+) = load(DocumentSource.toDocumentSource(uri), builder)
 
 /** Use bytearray as the pdf source, documents is not saved  */
-fun PDFView.load(
+inline fun PDFView.load(
     bytes: ByteArray,
     builder: PdfRequest.Builder.() -> Unit = {},
-) = load(ByteArraySource(bytes), builder)
+) = load(DocumentSource.toDocumentSource(bytes), builder)
 
 /** Use stream as the pdf source. Stream will be written to bytearray, because native code does not support Java Streams  */
-fun PDFView.load(
+inline fun PDFView.load(
     stream: InputStream,
     builder: PdfRequest.Builder.() -> Unit = {},
-) = load(InputStreamSource(stream), builder)
+) = load(DocumentSource.toDocumentSource(stream), builder)
--- pdfpreview/src/main/java/com/harissk/pdfpreview/source/DocumentSource.kt
+++ pdfpreview/src/main/java/com/harissk/pdfpreview/source/DocumentSource.kt
@@ -1,15 +1,52 @@
 package com.harissk.pdfpreview.source
 
 import android.content.Context
+import android.net.Uri
 import com.harissk.pdfium.PdfiumCore
+import java.io.File
 import java.io.IOException
+import java.io.InputStream
 
 
 /**
  * Created by Harishkumar on 25/11/23.
  */
 
+/**
+ * A functional interface representing a source for creating a PDF document.
+ * Implementations handle opening and loading the document from various sources.
+ */
 fun interface DocumentSource {
+
+    /**
+     * Creates a PDF document from this source.
+     *
+     * @param context The application context.
+     * @param core The PdfiumCore instance for document handling.
+     * @param password The password to open the document, if required.
+     *
+     * @throws IOException If an error occurs during document creation.
+     */
     @Throws(IOException::class)
     fun createDocument(context: Context, core: PdfiumCore, password: String?)
+
+    companion object {
+
+        /**
+         * Converts a supported object to its corresponding [DocumentSource] instance.
+         *
+         * @param source The object to convert.
+         * @return The appropriate [DocumentSource] instance for the given object.
+         * @throws IllegalArgumentException If the provided object is not a supported source type.
+         */
+        fun toDocumentSource(source: Any): DocumentSource = when (source) {
+            is String -> AssetSource(source)  // Assumes string represents an asset path
+            is File -> FileSource(source)
+            is Uri -> UriSource(source)
+            is ByteArray -> ByteArraySource(source)
+            is InputStream -> InputStreamSource(source)
+            is DocumentSource -> source
+            else -> throw IllegalArgumentException("Unsupported document source type: $source")
+        }
+    }
 }
